e5a8a9cab982b6f49a45ca315dbffeca091af002
Robin <rjblackborow@gmail.com>
2022-08-21 14:57:01 +0100

    refactored functions and included chrome tabs api

--- index.js
+++ index.js
@@ -2,12 +2,22 @@ let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
-let leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
+const tabBtn = document.getElementById("tab-btn")
+const deleteBtn = document.getElementById("delete-btn")
+const leadsFromLocalStorage = JSON.parse( localStorage.getItem("myLeads") )
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
     renderLeads()
 }
+
+tabBtn.addEventListener("click", function(){    
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
+        render(myLeads)
+    })
+})
 
 inputBtn.addEventListener("click", function() {
 
@@ -18,20 +28,26 @@ inputBtn.addEventListener("click", function() {
     
 })
 
-chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-    let url = tabs[0].url;
-    // use `url` here inside the callback because it's asynchronous!
-    inputEl.value = url;
-    console.log(url)
-});
+deleteBtn.addEventListener("click", function() {
+    localStorage.clear()
+    myLeads = []
+    render(myLeads)
+})
 
-function renderLeads() {
+// chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+//     let url = tabs[0].url;
+//     // use `url` here inside the callback because it's asynchronous!
+//     inputEl.value = url;
+//     console.log(url)
+// });
+
+function renderLeads(leads) {
     let listItems = ""
-    for (let i = 0; i < myLeads.length; i++) {
+    for (let i = 0; i < leads.length; i++) {
         listItems += `
             <li>
-                <a target='_blank' href='${myLeads[i]}'>
-                    ${myLeads[i]}
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
                 </a>
             </li>
         `
